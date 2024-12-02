c229c84f6d27f75ef0feb712d5a826416d0ca56f
mdakhlak <mohammadakhlak121@gmail.com>
2024-12-02 12:14:21 +0530

    Initial Commit

---
+++ Team15_SCMTests/Team15_SCMTests.swift
@@ -0,0 +1,17 @@
+//
+//  Team15_SCMTests.swift
+//  Team15_SCMTests
+//
+//  Created by Batch - 1 on 02/12/24.
+//
+
+import Testing
+@testable import Team15_SCM
+
+struct Team15_SCMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
